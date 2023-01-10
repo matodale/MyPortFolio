3b6e7417595c8709110df49cb6cd69f83b9dc1d2
matodale <sowo10@naver.com>
2023-01-11 00:28:57 +0900

    20230111 upload
    20230111 upload

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -3,8 +3,8 @@ const headerEl = document.querySelector("header");
 window.addEventListener('scroll', function(){
     const browerScrollX = window.innerWidth;
     const browerScrollY = window.pageYOffset;
-    console.log("x 값 : "+ browerScrollX);
-    console.log("y 값 : "+ browerScrollY);
+    // console.log("x 값 : "+ browerScrollX);
+    // console.log("y 값 : "+ browerScrollY);
     // 각 브라우저 가로 세로 크기에따라 header에 active 클래스 속성 추가
     if(browerScrollY > 800 || (browerScrollX < 737 && browerScrollY > 400) || (browerScrollX < 376 && browerScrollY > 300.5)){
         headerEl.classList.add("active");
